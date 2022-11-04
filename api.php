87e75df49f77fdfc6e81037baad0effa49cc3fb8
juanmacar15 <jdmadrid19@misena.edu.co>
2022-11-03 19:51:41 -0500

    ejercicio 3 avance

--- api.php
+++ api.php
@@ -3,32 +3,5 @@
     header('Content-Type: application/json; charset=utf-8');
     $_DATA = json_decode(file_get_contents("php://input"),true);
 
-
-    class productos{
-        public $codigo;
-        public $unidades;
-
-        public function __construct(int $codigo,int $unidades){
-            $this->codigo = $codigo;
-            $this->unidades = $unidades;
-        }   
-
-        public function codigos(){
-            $codigo = match($this->codigo){
-                10 => $codigo = 2000,
-                20 => $codigo = 1000,
-                30 => $codigo = 600,
-
-                default => $codigo = "codigo no valido"
-            };
-            return $codigo;
-        }
-        public function pagar():string{
-            $codigo = $this->codigos();
-            $pagar = $codigo*$this->unidades;
-            return $pagar;
-        }
-    }
-    $lista = new productos(codigo:$_DATA['codigo'],unidades:$_DATA['unidades']);
-    echo($lista->pagar());
+  
 ?>
--- main.js
+++ main.js
@@ -2,14 +2,7 @@ addEventListener("DOMContentLoaded",(e)=>{
     let myForm = document.querySelector("#Formulario");
     myForm.addEventListener("submit", async(e)=>{
        e.preventDefault();
-       let data = Object.fromEntries(new FormData(e.target));
-       let config = {
-           method: myForm.method,
-           body: JSON.stringify(data)
-       };
-       let peticion = await fetch(myForm.action,config);
-       let res = await peticion.text();
-       document.querySelector("#res").insertAdjacentHTML("beforeend",res);
+
 
     })
 })
